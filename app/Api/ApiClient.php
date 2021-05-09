7187a58d5d40c96a8c6f90267c49ff44f3b1d9d9
Asnex <asnex@yahoo.com>
2021-05-09 16:17:24 +0200

    ApiClient tests

---
+++ app/Api/ApiClient.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Api;
+
+class ApiClient {
+    protected $httpClient;
+
+    public function __construct($httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function getPostcodeData(string $postcode)
+    {
+        return $this->httpClient->get('postcodes/' . $postcode);
+    }
+
+    public function getPostcodesData(array $postcodes)
+    {
+        $postcodes = [
+            'json' => [
+                'postcodes' => $postcodes
+            ]
+        ];
+        return $this->httpClient->post('postcodes', $postcodes);
+    }
+}
--- tests/Unit/ApiClientTest.php
+++ tests/Unit/ApiClientTest.php
@@ -3,26 +3,30 @@
 namespace Tests\Unit;
 
 use GuzzleHttp\Client;
+use App\Api\ApiClient;
 use PHPUnit\Framework\TestCase;
 
 class
 ApiClientTest extends TestCase
 {
    protected $httpClient;
+   protected $apiClient;
 
    public function setUp(): void
    {
        $this->httpClient = new Client(['base_uri' => 'https://api.postcodes.io/']);
+       $this->apiClient = new ApiClient($this->httpClient);
    }
 
    public function tearDown(): void
    {
        $this->httpClient = null;
+       $this->apiClient = null;
    }
 
    public function test_show_postcode_data()
    {
-       $response = $this->httpClient->get('postcodes/NE30 1DP');
+       $response = $this->apiClient->getPostcodeData('NE30 1DP');
        $this->assertEquals(200, $response->getStatusCode());
        $data = (object) json_decode($response->getBody(), true);
        $this->assertArrayHasKey('country', $data->result);
@@ -30,12 +34,7 @@ ApiClientTest extends TestCase
 
     public function test_show_postcodes_data()
     {
-        $postcodes = [
-            'json' => [
-                'postcodes' => ["OX49 5NU", "M32 0JG", "NE30 1DP"]
-            ]
-        ];
-        $response = $this->httpClient->post('postcodes', $postcodes);
+        $response = $this->apiClient->getPostcodesData(["OX49 5NU", "M32 0JG", "NE30 1DP"]);
         $this->assertEquals(200, $response->getStatusCode());
         $data = (object) json_decode($response->getBody(), true);
         $this->assertCount(3, $data->result);
