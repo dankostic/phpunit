33fa3e3f7010d038774a6f45c06062667e7c321f
Asnex <asnex@yahoo.com>
2021-05-09 16:38:55 +0200

    MockHandler to use local data

--- tests/Unit/ApiClientTest.php
+++ tests/Unit/ApiClientTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use GuzzleHttp\Client;
 use App\Api\ApiClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 
 class
@@ -11,11 +13,17 @@ ApiClientTest extends TestCase
 {
    protected $httpClient;
    protected $apiClient;
+   protected $mockHandler;
 
    public function setUp(): void
    {
-       $this->httpClient = new Client(['base_uri' => 'https://api.postcodes.io/']);
+//       $this->httpClient = new Client(['base_uri' => 'https://api.postcodes.io/']);
+       $this->mockHandler = new MockHandler();
+       $this->httpClient = new Client([
+           'handler' => $this->mockHandler,
+       ]);
        $this->apiClient = new ApiClient($this->httpClient);
+
    }
 
    public function tearDown(): void
@@ -26,6 +34,8 @@ ApiClientTest extends TestCase
 
    public function test_show_postcode_data()
    {
+       $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__ . '/Data/postcode.json')));
+
        $response = $this->apiClient->getPostcodeData('NE30 1DP');
        $this->assertEquals(200, $response->getStatusCode());
        $data = (object) json_decode($response->getBody(), true);
@@ -34,6 +44,7 @@ ApiClientTest extends TestCase
 
     public function test_show_postcodes_data()
     {
+        $this->mockHandler->append(new Response(200, [], file_get_contents(__DIR__ . '/Data/postcodes.json')));
         $response = $this->apiClient->getPostcodesData(["OX49 5NU", "M32 0JG", "NE30 1DP"]);
         $this->assertEquals(200, $response->getStatusCode());
         $data = (object) json_decode($response->getBody(), true);
